d8b21b0f87af78e01550aea91545a65174c32312
Jeeva Prakash <jeevarag1016@gmail.com>
2023-12-15 11:05:44 +0530

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,45 @@
+import org.gradle.internal.impldep.bsh.commands.dir
+
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "tech.c1ph3rj.sample"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "tech.c1ph3rj.sample"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation(project(path = ":CurvedBottomNavigation"))
+}
